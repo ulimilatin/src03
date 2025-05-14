73dafffc27b98c02e6d608e7ef19b12c70294ecd
Uli Milatin <ulimilatin@gmail.com>
2025-05-14 11:57:07 +0700

    menambah 3 file baru

---
+++ Ulasan.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ulasan;
+
+/**
+ *
+ * @author MyBook Hype
+ */
+public class Ulasan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Flora kelengkeng = new Flora();
+        System.out.println("Nama Pohon :" + kelengkeng.nama);
+        Rambutan rambutan01= new Rambutan();
+        System.out.println("Jenis Rambutan :" + rambutan01.nama);
+    }
+    
+}
